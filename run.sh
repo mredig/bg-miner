b310ca891808c8125dc41bf27cbbbaa12b40914c
Michael Redig <mredig@gmail.com>
2019-02-07 21:27:17 -0600

    auto nice to autogroup

--- run.sh
+++ run.sh
@@ -13,9 +13,10 @@ else
 
 	cpulimit -l $LIMIT -p $XMRPID -b
 
-	echo 19 > /proc/$XMRPID/autogroup
 
 	# let the bg process print to screen and then give you a clean prompt
 	sleep 2
+	echo 19 > /proc/$XMRPID/autogroup
+
 	echo
 fi
